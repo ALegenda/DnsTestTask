fe4cbc235863826b3414928bb0f3a05d2d2e7e92
TomKuper <adalekseev@edu.hse.ru>
2019-07-26 15:43:35 +1000

    Completed task without command line arguments

--- DnsTestTask/Program.cs
+++ DnsTestTask/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace DnsTestTask
 {
@@ -10,12 +7,28 @@ namespace DnsTestTask
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            PrintFiles(new DirectoryInfo(Directory.GetCurrentDirectory()), 0);
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        private static void PrintFiles(DirectoryInfo info, int nesting)
+        {
+            Console.WriteLine($"{Indent(nesting)}└───{info.Name}");
+
+            foreach (var fileInfo in info.GetFiles())
+            {
+                Console.WriteLine($"{Indent(nesting + 1)}├───{fileInfo.Name} ({fileInfo.Length} B)");
+            }
+
+            foreach (var directoryInfo in info.GetDirectories())
+            {
+                PrintFiles(directoryInfo, nesting + 1);
+            }
+
+        }
+
+        public static string Indent(int count)
+        {
+            return "".PadLeft(count * 4);
         }
     }
 }
